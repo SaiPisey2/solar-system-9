9132ee118ef5acd54820fec9246595dd70bde391
SaiPisey2 <piseysai0202@gmail.com>
2023-05-14 07:45:49 -0400

    chnage

--- pr.sh
+++ pr.sh
@@ -1,4 +1,4 @@
 echo "Opening a Pull Request"
 
-curl -X POST -H "Authorization: token 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" -d "{\\"title\\":\\"My Pull Request Title\\",\\"head\\":\\"feature-gitea\\",\\"base\\":\\"main\\"}" https://api.github.com/repos/SaiPisey2/gitops-argocd/pulls
+curl -X POST -H "Authorization: token $ARGOCD_TOKEN" -d "{\\"title\\":\\"My Pull Request Title\\",\\"head\\":\\"feature-gitea\\",\\"base\\":\\"main\\"}" https://api.github.com/repos/SaiPisey2/gitops-argocd/pulls
 echo "Success only 2"
