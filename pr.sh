b12c3b793c66d022b4919395fccfc9e6eeebf924
SaiPisey2 <piseysai0202@gmail.com>
2023-05-14 07:44:37 -0400

    chnages

--- pr.sh
+++ pr.sh
@@ -1,11 +1,4 @@
 echo "Opening a Pull Request"
 
-curl -L \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: $ARGOCD_TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/api/v1/repos/SaiPisey2/gitops-argocd/pulls \
-  -d '{"title":"Amazing new feature","body":"Please pull these awesome changes in!","head":"feature-gitea","base":"main"}'
-
-echo "Success only"
+curl -X POST -H "Authorization: token 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" -d "{\\"title\\":\\"My Pull Request Title\\",\\"head\\":\\"feature-gitea\\",\\"base\\":\\"main\\"}" https://api.github.com/repos/SaiPisey2/gitops-argocd/pulls
+echo "Success only 2"
