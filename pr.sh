b4197c535156a0490bfecc617f1d762124ea6679
SaiPisey2 <piseysai0202@gmail.com>
2023-05-14 07:39:51 -0400

    change

--- pr.sh
+++ pr.sh
@@ -3,7 +3,7 @@ echo "Opening a Pull Request"
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: $jenkins-git" \
+  -H "Authorization: $ARGOCD_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/api/v1/repos/SaiPisey2/gitops-argocd/pulls \
   -d '{"title":"Amazing new feature","body":"Please pull these awesome changes in!","head":"feature-gitea","base":"main"}'
