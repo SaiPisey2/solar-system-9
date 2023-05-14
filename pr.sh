a0226b0e554d20cf8c572521b3cb38c4cacb5c7f
SaiPisey2 <piseysai0202@gmail.com>
2023-05-14 07:51:19 -0400

    asd

--- pr.sh
+++ pr.sh
@@ -1,4 +1,4 @@
 echo "Opening a Pull Request"
 
-curl -X POST -H "Authorization: $ARGOCD_TOKEN" -d "{\\"title\\":\\"My Pull Request Title\\",\\"head\\":\\"feature-gitea\\",\\"base\\":\\"main\\"}" https://api.github.com/repos/SaiPisey2/gitops-argocd/pulls
+curl -X POST -H "Authorization: bearer $ARGOCD_TOKEN" -d "{\\"title\\":\\"My Pull Request Title\\",\\"head\\":\\"feature-gitea\\",\\"base\\":\\"main\\"}" https://api.github.com/repos/SaiPisey2/gitops-argocd/pulls
 echo "Success only 2"
