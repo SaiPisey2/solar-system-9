cec93c4ad5bc68c614ab9ae02c391c661269e710
SaiPisey2 <piseysai0202@gmail.com>
2023-05-14 07:37:03 -0400

    change

--- pr.sh
+++ pr.sh
@@ -5,7 +5,7 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: $jenkins-git" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/SaiPisey2/gitops-argocd/pulls \
+  https://api.github.com/api/v1/repos/SaiPisey2/gitops-argocd/pulls \
   -d '{"title":"Amazing new feature","body":"Please pull these awesome changes in!","head":"feature-gitea","base":"main"}'
 
 echo "Success only"
