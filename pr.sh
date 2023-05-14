b0d13355d67170365073d3079a4041228ed45e8a
SaiPisey2 <piseysai0202@gmail.com>
2023-05-14 09:12:31 -0400

    changes to pr

--- pr.sh
+++ pr.sh
@@ -1,19 +1,13 @@
 echo "Opening a Pull Request"
 
-curl -X 'POST' \
-  'https://api.github.com/repos/SaiPisey2/gitops-argocd/pulls' \
-  -H 'accept: APPLICATION_JSON' \
-  -H "authorization: $ARGOCD_TOKEN" \
-  -H 'Content-Type: APPLICATION_JSON' \
-  -d '{
-  "assignee": "SaiPisey2",
-  "assignees": [
-    "SaiPisey2"
-  ],
-  "base": "main",
-  "body": "Updated deployment specification with a new image version.",
-  "head": "feature-gitea",
-  "title": "Updated Solar System Image"
-}'
+gh api \
+  --method POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  /repos/SaiPisey2/gitops-argocd/pulls \
+  -f title='Amazing new feature yes!' \
+ -f body='Please pull these awesome changes in!' \
+ -f head='feature-gitea' \
+ -f base='main'
 
 echo "Success"
