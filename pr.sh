4cdc6b064857c382b98108841c6c9c812bd9246a
SaiPisey2 <piseysai0202@gmail.com>
2023-05-14 07:54:11 -0400

    asd

--- pr.sh
+++ pr.sh
@@ -1,4 +1,19 @@
 echo "Opening a Pull Request"
 
-curl -X POST -H "Authorization: bearer $ARGOCD_TOKEN" -d "{\\"title\\":\\"My Pull Request Title\\",\\"head\\":\\"feature-gitea\\",\\"base\\":\\"main\\"}" https://api.github.com/repos/SaiPisey2/gitops-argocd/pulls
-echo "Success only 2"
+curl -X 'POST' \
+  'https://api.github.com/repos/SaiPisey2/gitops-argocd/pulls' \
+  -H 'accept: APPLICATION_JSON' \
+  -H "authorization: token $ARGOCD_TOKEN" \
+  -H 'Content-Type: APPLICATION_JSON' \
+  -d '{
+  "assignee": "SaiPisey2",
+  "assignees": [
+    "SaiPisey2"
+  ],
+  "base": "main",
+  "body": "Updated deployment specification with a new image version.",
+  "head": "feature-gitea",
+  "title": "Updated Solar System Image"
+}'
+
+echo "Success"
