9eb17b823603a2322fdbacb444bf2182f0a80c59
SaiPisey2 <piseysai0202@gmail.com>
2023-05-14 07:30:01 -0400

    chnages

--- pr.sh
+++ pr.sh
@@ -1,19 +1,11 @@
 echo "Opening a Pull Request"
 
-curl -X 'POST' \
-  'https://api.github.com/repos/SaiPisey2/gitops-argocd/pulls' \
-  -H 'accept: application/vnd.github+json' \
-  -H "authorization: $ARGOCD_TOKEN" \
-  -H 'Content-Type: application/vnd.github+json' \
-  -d '{
-  "assignee": "SaiPisey2",
-  "assignees": [
-    "SaiPisey2"
-  ],
-  "base": "main",
-  "body": "Updated deployment specification with a new image version.",
-  "head": "feature-gitea",
-  "title": "Updated Solar System Image"
-}'
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: $ARGOCD_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/SaiPisey2/gitops-argocd/pulls \
+  -d '{"title":"Amazing new feature","body":"Please pull these awesome changes in!","head":"feature-gitea","base":"main"}'
 
 echo "Success"
